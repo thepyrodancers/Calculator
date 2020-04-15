cb34584e86a022b2f97585c5a22f12f2fcb800e3
Aaron Sumpter <awsumpter@gmail.com>
2020-04-15 00:05:04 -0500

    Working Calculator code

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,8 +1,6 @@
 
-//
-// This is example code from Chapter 6.6 "Trying the first version" of
-// "Software - Principles and Practice using C++" by Bjarne Stroustrup
-//
+
+// Aaron's Calculator Code 
 
 #include "Facilities.h"
 
@@ -20,24 +18,57 @@ public:
 
 //------------------------------------------------------------------------------
 
-Token get_token()    // read a token from cin
-{
-    char ch;
-    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+class Token_stream {
+public:
+    Token_stream(); // make a Token_stream that reads from cin
+    Token get(); // get a Token
+    void putback(Token t); // put a Token back
+private:
+    bool full{ false }; // is there a Token in the buffer?
+    Token buffer; // here is where we keep a Token put back using putback()
 
+};
+
+
+//------------------------------------------------------------------------------
+
+// The constructor just sets full to indicate that the buffer is empty:
+Token_stream::Token_stream()
+    :full(false), buffer(0)    // no Token in buffer
+{
+}
+
+//------------------------------------------------------------------------------
+
+void Token_stream::putback(Token t)
+{
+    if (full) error("putback() into a full buffer");
+    buffer = t; // copy t to buffer
+    full = true; // buffer is now full
+}
+
+//------------------------------------------------------------------------------
+
+Token Token_stream::get()
+{
+    if (full) { // do we already have a Token ready?
+        full = false; // remove Token from buffer
+        return buffer;
+    }
+    char ch;
+    cin >> ch; // note that >> skips whitespace (space, newline, tab, etc.)
     switch (ch) {
-        //not yet   case ';':    // for "print"
-        //not yet   case 'q':    // for "quit"
+    case '=': // for “print”
+    case 'x': // for “quit”
     case '(': case ')': case '+': case '-': case '*': case '/':
-        return Token(ch);        // let each character represent itself
+        return Token{ ch }; // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
-    {
-        cin.putback(ch);         // put digit back into the input stream
-        double val;
-        cin >> val;              // read a floating-point number
-        return Token('8', val);   // let '8' represent "a number"
+    { cin.putback(ch); // put digit back into the input stream
+    double val;
+    cin >> val; // read a floating-point number
+    return Token{ 'n',val }; // let ‘8’ represent “a number”
     }
     default:
         error("Bad token");
@@ -46,37 +77,102 @@ Token get_token()    // read a token from cin
 
 //------------------------------------------------------------------------------
 
-double expression();  // read and evaluate a Expression
+Token_stream ts;       // provides get() and putback()
 
 //------------------------------------------------------------------------------
 
-double term();        // read and evaluate a Term
+double expression();  // read and evaluate a Expression
 
 //------------------------------------------------------------------------------
 
 double primary()     // read and evaluate a Primary
 {
-    Token t = get_token();
+    Token t = ts.get();
     switch (t.kind) {
     case '(':    // handle '(' expression ')'
     {
         double d = expression();
-        t = get_token();
+        t = ts.get();
         if (t.kind != ')') error("')' expected");
         return d;
     }
-    case '8':            // we use '8' to represent a number
+    case 'n':            // we use '8' to represent a number
         return t.value;  // return the number's value
     default:
         error("primary expected");
     }
 }
+
+//------------------------------------------------------------------------------
+
+double term()
+{
+    double left = primary();
+    Token t = ts.get(); // get the next Token from the Token stream
+    while (true) {
+        switch (t.kind) {
+        case '*':
+            left *= primary();
+            t = ts.get();
+            break;
+        case '/':
+        { double d = primary();
+        if (d == 0) error("divide by zero");
+        left /= d;
+        t = ts.get();
+        break;
+        }
+        default:
+            ts.putback(t); // put t back into the Token stream
+            return left;
+        }
+    }
+}
+
+
+//------------------------------------------------------------------------------
+
+double expression()
+{
+    double left = term(); // read and evaluate a Term
+    Token t = ts.get(); // get the next Token from the Token stream
+    while (true) {
+        switch (t.kind) {
+        case '+':
+            left += term(); // evaluate Term and add
+            t = ts.get();
+            break;
+        case '-':
+            left -= term(); // evaluate Term and subtract
+            t = ts.get();
+            break;
+        default:
+            ts.putback(t); // put t back into the token stream
+            return left; // finally: no more + or –; return the answer
+        }
+    }
+}
+
 //------------------------------------------------------------------------------
 
 int main()
 try {
-    while (cin)
-        cout << expression() << '\n';
+
+    cout << "Welcome to our simple calculator. \nPlease enter expressions using floating-point numbers.\n";
+    cout << "You may use '+' for addition, '-' for subtraction, '*' for multiplication, and '/' for division.\n";
+    cout << "You may use '(' and ')' to denote modifications to the normal order of mathematical operations.\n";
+    cout << "End your expressions with '=' to get the result.\n\n";
+    cout << ">";
+    double val = 0;
+    while (cin) {
+        Token t = ts.get();
+        if (t.kind == 'x') break; // ‘x’ for “quit”
+        if (t.kind == '=') // ‘;’ for “print now”
+            cout << " " << val << '\n';
+        else
+            ts.putback(t);
+        val = expression();
+    }
     keep_window_open("~0");
 }
 catch (exception& e) {
@@ -89,54 +185,3 @@ catch (...) {
     keep_window_open("~2");
     return 2;
 }
-
-//------------------------------------------------------------------------------
-
-double expression()
-{
-    double left = term();      // read and evaluate a Term
-    Token t = get_token();     // get the next token
-    while (true) {
-        switch (t.kind) {
-        case '+':
-            left += term();    // evaluate Term and add
-            t = get_token();
-            break;
-        case '-':
-            left -= term();    // evaluate Term and subtract
-            t = get_token();
-            break;
-        default:
-            return left;       // finally: no more + or -: return the answer
-        }
-    }
-}
-
-//------------------------------------------------------------------------------
-
-double term()
-{
-    double left = primary();
-    Token t = get_token();     // get the next token
-
-    while (true) {
-        switch (t.kind) {
-        case '*':
-            left *= primary();
-            t = get_token();
-            break;
-        case '/':
-        {
-            double d = primary();
-            if (d == 0) error("divide by zero");
-            left /= d;
-            t = get_token();
-            break;
-        }
-        default:
-            return left;
-        }
-    }
-}
-
-//------------------------------------------------------------------------------
