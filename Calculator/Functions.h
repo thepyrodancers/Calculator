7c9c06964be3ce822f3bdcb5de718a80858c7396
David Dickinson <davrdic@gmail.com>
2020-05-13 23:07:50 -0500

    functions.h declarations

---
+++ Calculator/Functions.h
@@ -0,0 +1,14 @@
+#pragma once
+void calculate();
+double primary();
+double factorial();
+double term();
+double expression();
+double calculate_factorial(int val);
+double get_value(string s);
+void set_value(string s, double d);
+void print_intro();
+double statement();
+bool is_declared(string var);
+double define_name(string var, double val);
+double declaration();
