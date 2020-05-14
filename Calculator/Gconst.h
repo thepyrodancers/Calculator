1f0c448249d4d5cc46d56f302d8c408d1c23c894
David Dickinson <davrdic@gmail.com>
2020-05-13 21:51:39 -0500

    added all but string conts

--- Calculator/Gconst.h
+++ Calculator/Gconst.h
@@ -2,3 +2,10 @@
 
 #include <string>
 
+const char number = 'n';
+const char quit = 'q';
+const char print = ';';
+const char name = 'a';
+const char let = 'L';
+const char square = 'S';
+const char power = 'P';
